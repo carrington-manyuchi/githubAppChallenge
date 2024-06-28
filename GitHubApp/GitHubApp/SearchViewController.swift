b181635fd75d67558018944d72419bdb3040d71b
Carrington Manyuchi <carrington.manyuchi@standardbank.co.za>
2024-06-28 03:32:10 +0200

    Added SearchViewController

--- GitHubApp/GitHubApp/SearchViewController.swift
+++ GitHubApp/GitHubApp/SearchViewController.swift
@@ -9,18 +9,60 @@ import UIKit
 
 class SearchViewController: UIViewController {
     
-    let logoImageView = UIImageView()
+    private let logoImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.image = UIImage(named: "gh-logo")
+        return imageView
+    }()
+    
     let usernameTextField = GFTextField()
     let callToActionButton = GFButton(backgroundColor: .systemGreen, title: "Get Followers")
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
+        configureViews()
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         navigationController?.isNavigationBarHidden = true
+    }
+    
+    func configureViews() {
+        view.addSubview(logoImageView)
+        view.addSubview(usernameTextField)
+        view.addSubview(callToActionButton)
+        configureConstraints()
+    }
+    
+    func configureConstraints() {
+        let logoImageViewConstraints = [
+            logoImageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 80),
+            logoImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            logoImageView.heightAnchor.constraint(equalToConstant: 200),
+            logoImageView.widthAnchor.constraint(equalToConstant: 200)
+        ]
+        
+        let usernameTextFieldConstraints = [
+            usernameTextField.topAnchor.constraint(equalTo: logoImageView.bottomAnchor, constant: 48),
+            usernameTextField.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 50),
+            usernameTextField.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -50),
+            usernameTextField.heightAnchor.constraint(equalToConstant: 50)
+        ]
+        
+        let callToActionButtonConstraints = [
+            callToActionButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -50),
+            callToActionButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            callToActionButton.leadingAnchor.constraint(equalTo: usernameTextField.leadingAnchor),
+            callToActionButton.trailingAnchor.constraint(equalTo: usernameTextField.trailingAnchor),
+            callToActionButton.heightAnchor.constraint(equalToConstant: 50)
+        ]
+        
+        NSLayoutConstraint.activate(logoImageViewConstraints)
+        NSLayoutConstraint.activate(usernameTextFieldConstraints)
+        NSLayoutConstraint.activate(callToActionButtonConstraints)
     }
 
 }
